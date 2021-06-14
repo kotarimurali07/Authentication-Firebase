9b8570f57746444ab557511586f734b844062778
kotarimurali07 <kotarimurali07@gmail.com>
2021-06-14 22:55:06 +0530

    TICKEWT-3

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import TopNavBar from "./services/Dashboard/components/TopNavBar";
 import WebCamera from "./services/Dashboard/components/WebCamera";
+import Charts from "./services/Dashboard/components/Charts";
 function App() {
   return (
     <div className="App">
@@ -18,6 +19,7 @@ function App() {
             <Route path="/ForgotPassword" component={ForgotPassword} />
             <Route exact path="/" component={TopNavBar} />
             <Route path="/WebCamera" component={WebCamera} />
+            <Route path="/Charts" component={Charts} />
           </Switch>
         </Provider>
       </Router>
