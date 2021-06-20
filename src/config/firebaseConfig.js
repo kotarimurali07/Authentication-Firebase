15cdc07d8e02e83eb8a5248d4a2d5ae0edecb658
Murali Kotari <83963142+kotarimurali07@users.noreply.github.com>
2021-06-20 19:29:34 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,12 +3,7 @@ import "firebase/auth";
 import "firebase/storage";
 import "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAGCXdrpRjsdDAn9Q8AJ9J1tRKBHnbvOi8",
-  authDomain: "unichat-316f1.firebaseapp.com",
-  projectId: "unichat-316f1",
-  storageBucket: "unichat-316f1.appspot.com",
-  messagingSenderId: "34845760798",
-  appId: "1:34845760798:web:7ef25dd479c81d7614fd0f",
+
 };
 
 firebase.initializeApp(firebaseConfig);
